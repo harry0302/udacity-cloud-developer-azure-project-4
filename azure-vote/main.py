ab0bafa6e43db06841f73d911c3ab2cb794bf1d7
kenji0305 <kenji@cet-app.com>
2024-11-01 17:46:13 +0700

    update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -6,48 +6,74 @@ import socket
 import sys
 import logging
 from datetime import datetime
-from opencensus.ext.azure.log_exporter import AzureLogHandler, AzureEventHandler
+
+# App Insights
+# Import required libraries for App Insights
 from opencensus.ext.azure import metrics_exporter
+from opencensus.ext.azure.log_exporter import AzureEventHandler, AzureLogHandler
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 from opencensus.stats import aggregation as aggregation_module
 from opencensus.stats import measure as measure_module
 from opencensus.stats import stats as stats_module
 from opencensus.stats import view as view_module
 from opencensus.tags import tag_map as tag_map_module
-from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace import config_integration
 from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
-from opencensus.ext.flask.flask_middleware import FlaskMiddleware
-# App Insights
-# TODO: Import required libraries for App Insights
-
-# Logging
-logger = logging.getLogger(__name__)# TODO: Setup logger
-logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=d6fe0e6c-2493-4117-9396-fea4165ce36d;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=1bfa57ad-040d-40db-8de8-5d1884715947'))
-logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=d6fe0e6c-2493-4117-9396-fea4165ce36d;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=1bfa57ad-040d-40db-8de8-5d1884715947'))
 
 # Metrics
+stats = stats_module.stats
+view_manager = stats.view_manager
+
+config_integration.trace_integrations(["logging"])
+config_integration.trace_integrations(["requests"])
+
+# Metrics
+
+
+# Logging
+logger = logging.getLogger(__name__) # Setup logger
+handler = AzureLogHandler(
+    connection_string="InstrumentationKey=d6fe0e6c-2493-4117-9396-fea4165ce36d;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=1bfa57ad-040d-40db-8de8-5d1884715947"
+)
+
+handler.setFormatter(logging.Formatter("%(traceId)s %(spanId)s %(message)s"))
+
+logger.addHandler(handler)
+logger.addHandler(
+    AzureEventHandler(
+        connection_string="InstrumentationKey=d6fe0e6c-2493-4117-9396-fea4165ce36d;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=1bfa57ad-040d-40db-8de8-5d1884715947"
+    )
+)
+
+logger.setLevel(logging.INFO)
+
 exporter = metrics_exporter.new_metrics_exporter(
-  enable_standard_metrics=True,
-  connection_string='InstrumentationKey=d6fe0e6c-2493-4117-9396-fea4165ce36d;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=1bfa57ad-040d-40db-8de8-5d1884715947')
-# TODO: Setup exporter
+    enable_standard_metrics=True,
+    connection_string="InstrumentationKey=d6fe0e6c-2493-4117-9396-fea4165ce36d;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=1bfa57ad-040d-40db-8de8-5d1884715947",
+) # Setup exporter
+
+view_manager.register_exporter(exporter)
 
 # Tracing
 tracer = Tracer(
     exporter=AzureExporter(
-        connection_string='InstrumentationKey=d6fe0e6c-2493-4117-9396-fea4165ce36d;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=1bfa57ad-040d-40db-8de8-5d1884715947'),
+        connection_string="InstrumentationKey=d6fe0e6c-2493-4117-9396-fea4165ce36d;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=1bfa57ad-040d-40db-8de8-5d1884715947"
+    ),
     sampler=ProbabilitySampler(1.0),
-)
-# TODO: Setup tracer
+) # Setup tracer
 
 app = Flask(__name__)
 
 # Requests
-middleware =  FlaskMiddleware(
+middleware = FlaskMiddleware(
     app,
-    exporter=AzureExporter(connection_string="InstrumentationKey=d6fe0e6c-2493-4117-9396-fea4165ce36d;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=1bfa57ad-040d-40db-8de8-5d1884715947"),
+    exporter=AzureExporter(
+        connection_string="InstrumentationKey=d6fe0e6c-2493-4117-9396-fea4165ce36d;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=1bfa57ad-040d-40db-8de8-5d1884715947"
+    ),
     sampler=ProbabilitySampler(rate=1.0),
-)
-# TODO: Setup flask middleware
+) # Setup flask middleware
 
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
@@ -68,28 +94,18 @@ else:
     title = app.config['TITLE']
 
 # Redis Connection
-if ("REDIS" in os.environ and os.environ['REDIS']):
-    redis_server = os.environ['REDIS']
-else:
-    redis_server = app.config['REDIS']
-try:
-   if "REDIS_PWD" in os.environ:
-      r = redis.StrictRedis(host=redis_server,
-                        port=6379,
-                        password=os.environ['REDIS_PWD'])
-   else:
-      r = redis.Redis(redis_server)
-   r.ping()
-except redis.ConnectionError:
-   exit('Failed to connect to Redis, terminating.')
+r = redis.Redis()
 
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
     title = socket.gethostname()
 
 # Init Redis
-if not r.get(button1): r.set(button1,0)
-if not r.get(button2): r.set(button2,0)
+if not r.get(button1):
+    r.set(button1,0)
+    
+if not r.get(button2):
+    r.set(button2,0)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -98,11 +114,14 @@ def index():
 
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
-        tracer.span(name="Total {} Voted: {}".format(button1, vote1))
-        # TODO: use tracer object to trace cat vote
+        # use tracer object to trace cat vote
+        with tracer.span(name="Cats Vote") as span:
+            print("Cats Vote")
+            
         vote2 = r.get(button2).decode('utf-8')
-        tracer.span(name="Total {} Voted: {}".format(button2, vote2))
-        # TODO: use tracer object to trace dog vote
+        # use tracer object to trace dog vote
+        with tracer.span(name="Dogs Vote") as span:
+            print("Dogs Vote")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -116,13 +135,13 @@ def index():
             r.set(button2,0)
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
-            logger.warning("{} voted".format(button1), extra=properties)
-            # TODO: use logger object to log cat vote
-            
+            # use logger object to log cat vote
+            logger.info("Cats Vote", extra=properties)
+
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            logger.warning("{} voted".format(button2), extra=properties)
-            # TODO: use logger object to log dog vote
+            # use logger object to log dog vote
+            logger.info("Dogs Vote", extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
@@ -140,7 +159,7 @@ def index():
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
-    # TODO: Use the statement below when running locally
+    # Use the statement below when running locally
     # app.run() 
-    # TODO: Use the statement below before deployment to VMSS
-    app.run(host='0.0.0.0', threaded=True, debug=True) # remote
+    # Use the statement below before deployment to VMSS
+    app.run(host='0.0.0.0', threaded=True, debug=False, port=5000) # remote
